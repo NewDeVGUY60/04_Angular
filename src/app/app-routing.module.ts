c45f4012af7a17cb3a6d466ce2ecc1f20ed7fcec
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2023-01-10 14:19:22 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: UserProfileComponent},
+  {path:'signup', component: SignUpComponent },
+  {path:'user', component: UserProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { };
+export {routes}
